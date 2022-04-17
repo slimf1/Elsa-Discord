4bc737335a0e0348ead2582cd5caf821329292cc
slimf1 <slimane.f41@gmail.com>
2022-04-17 15:41:58 +0200

    Added info command

---
+++ src/commands/dev/info.ts
@@ -0,0 +1,15 @@
+import { Message } from 'discord.js';
+import { IBot } from '../../bot';
+import Command from '../../command';
+
+export default class Info extends Command {
+  async execute(bot: IBot, message: Message<boolean>): Promise<void> {
+    const botTag = bot.client.user?.tag;
+    const maintainerTag = bot.client.users.cache.get('303548403520897025')?.tag;
+    await message
+      .reply(`${botTag}: Bot inutile développé et maintenu par ${maintainerTag}.`);
+  }
+  name(): string {
+    return 'info';
+  }
+}
